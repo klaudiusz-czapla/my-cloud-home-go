39984f72b501e99836b3b21684bc1e83a1837c34
Klaudiusz Czapla <klaudiuszc@interia.pl>
2022-12-05 22:07:36 +0100

    up

--- main.go
+++ main.go
@@ -11,14 +11,19 @@ const (
 	version = "0.0.1"
 )
 
-var (
-	configFile string
-)
-
 var rootCmd = &cobra.Command{
 	Use:   "my-cloud-home-go",
 	Short: "My Cloud Home CLI application",
 	Long:  `my-cloud-home-go is meant for managing My Cloud Home devices`,
+}
+
+var tokenCmd = &cobra.Command{
+	Use:   "token",
+	Short: "",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
 }
 
 var versionCmd = &cobra.Command{
@@ -30,22 +35,15 @@ var versionCmd = &cobra.Command{
 	},
 }
 
-var tokenCmd = &cobra.Command{
-	Use:   "token",
-	Short: "",
-	Long:  "",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
-	},
-}
-
 func main() {
 
-	tokenCmd.LocalFlags().StringP("username", "u", "", "WD My Cloud Home user name")
-	tokenCmd.LocalFlags().StringP("password", "p", "", "WD My Cloud Home user password")
+	rootCmd.PersistentFlags().StringP("username", "u", "", "WD My Cloud Home user name")
+	rootCmd.PersistentFlags().StringP("password", "p", "", "WD My Cloud Home user password")
+	rootCmd.MarkFlagsRequiredTogether("username", "password")
 
-	viper.BindPFlag("username", tokenCmd.LocalFlags().Lookup("username"))
-	viper.BindPFlag("password", tokenCmd.LocalFlags().Lookup("password"))
+	viper.BindPFlag("username", rootCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("password", rootCmd.PersistentFlags().Lookup("password"))
+	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
 
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(tokenCmd)
---
+++ makefile
@@ -0,0 +1,5 @@
+build:
+	go build -o my-cloud-home-go main.go
+
+run:
+	go run main.go
