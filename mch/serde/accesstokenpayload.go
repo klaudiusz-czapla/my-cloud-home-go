eac3d783d9199626e3ba82ca282cc6711ec9f5ee
Klaudiusz Czapla <klaudiuszc@interia.pl>
2022-12-23 17:57:46 +0100

    access token payload contract for deserialization

--- mch/serde/accesstokenpayload.go
+++ mch/serde/accesstokenpayload.go
@@ -5,7 +5,14 @@ import (
 )
 
 type AccessTokenPayload struct {
-	Exp int64
+	Iss   string   `json:"iss"`
+	Sub   string   `json:"sub"`
+	Aud   []string `json:"aud"`
+	Iat   int64    `json:"iat"`
+	Exp   int64    `json:"exp"`
+	Azp   string   `json:"azp"`
+	Scope string   `json:"scope"`
+	Gty   string   `json:"gty"`
 }
 
 // maybe better will be to check whether we are about to expire soon (like few minutes before expiration time at least)
